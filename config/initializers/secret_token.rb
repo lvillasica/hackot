2b04ce36d0e010799eb6e07c96e24a9223e8ad8d
Leo Martin Villasica <exst_lvillasica@yahoo.com>
2013-11-16 13:08:19 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackot::Application.config.secret_token = 'b1cf387304c5f41bab0455d0139ac223d4cd9515f68fed9aee6f99931b9db49b67a43d97612fb05b0039352af09579c6496ae34af4fc413b2e5cca9b3876d096'
