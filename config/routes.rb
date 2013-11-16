b316b0885030807cb03ee28795f3ebac41e12e28
Stefany Serino <stefanyserino@gmail.com>
2013-11-17 04:36:56 +0800

    move users under events

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Hackot::Application.routes.draw do
   get '/logout' => 'sessions#destroy'
 
   resources :events, :only => [:index, :show, :create, :update]
-  resources :relief_centers, :only => [:index, :show, :update] do
-    member do
-      get :timesheet
-    end
+  resources :relief_centers, :only => [:index, :show, :update]
 
-    resources :events, :only => [:index, :show]
+  resources :events, :only => [:index, :show] do
+    member do
+      get :users
+    end
   end
+
   resources :users, :only => [:index, :show, :update]
 
   root :to => 'board#index'
